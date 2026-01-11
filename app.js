b67b9b6123f612439b92ce8afefb6d14f1fd21cd
Krishna <patelkrishna1922@gmail.com>
2026-01-11 23:15:55 +0530

    Change server port to 8080

--- app.js
+++ app.js
@@ -100,9 +100,8 @@ app.use((err, req, res, next) => {
 
 
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(8080, () => {
+    console.log("Server is listening to port 8080");
 });
+
 
